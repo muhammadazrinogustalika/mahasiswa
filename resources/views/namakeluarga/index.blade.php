053783ce0994db8c102b1ac67778f9315c11bb28
VIP <azrino@ittelkom-pwt.ac.id>
2025-10-01 15:09:36 +0700

    first commit

--- resources/views/namakeluarga/index.blade.php
+++ resources/views/namakeluarga/index.blade.php
@@ -14,7 +14,7 @@
                         <h3 class="text-2xl font-semibold text-gray-900 dark:text-gray-100">Daftar Data Keluarga Mahasiswa</h3>
                         <a href="{{ route('namakeluarga.create') }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                             <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
-                            Tambah Data Keluarga Mahasiswa
+                            Tambah Data Keluarga
                         </a>
                     </div>
 
@@ -29,37 +29,38 @@
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
                                     <th scope="col" class="py-3 px-6">No.</th>
+                                    <th scope="col" class="py-3 px-6">NIM Mahasiswa</th>
+                                    <th scope="col" class="py-3 px-6">Nama Mahasiswa</th>
                                     <th scope="col" class="py-3 px-6">Nama Ayah</th>
-                                    <th scope="col" class="py-3 px-6">Hubungan Ayah</th>
                                     <th scope="col" class="py-3 px-6">Nama Ibu</th>
-                                    <th scope="col" class="py-3 px-6">Hubungan Ibu</th>
-                                    <th scope="col" class="py-3 px-6">buktikk</th>
+                                    <th scope="col" class="py-3 px-6">Bukti KK</th>
                                     <th scope="col" class="py-3 px-6 text-center">Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                @forelse ($keluarga as $index => $namakeluarga)
+                                @forelse ($keluarga as $index => $item)
                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                     <td class="py-4 px-6">{{ $keluarga->firstItem() + $index }}</td>
+                                    <td class="py-4 px-6 font-medium text-gray-900 dark:text-white">{{ $item->NIM }}</td>
+                                    {{-- [PERBAIKAN] Mengambil nama mahasiswa melalui relasi yang sudah dibuat di model --}}
+                                    <td class="py-4 px-6">{{ $item->mahasiswa->NAMA ?? 'N/A' }}</td>
+                                    <td class="py-4 px-6">{{ $item->NAMA_AYAH }}</td>
+                                    <td class="py-4 px-6">{{ $item->NAMA_IBU }}</td>
                                     <td class="py-4 px-6">
-                                        @if ($namakeluarga->buktikk)
-                                            {{-- [PERBAIKAN] Typo pada variabel $mahsiswanama menjadi $mahasiswanama --}}
-                                            <img class="w-10 h-10 rounded-md object-cover" src="{{ asset('storage/' . $namakeluarga->buktikk) }}" alt="{{ $namakeluarga->NAMA }}">
+                                        @if ($item->buktikk)
+                                            <a href="{{ asset('storage/' . $item->buktikk) }}" target="_blank" class="text-indigo-600 dark:text-indigo-400 hover:underline">
+                                                Lihat File
+                                            </a>
                                         @else
-                                            <div class="w-10 h-10 rounded-md bg-gray-200 dark:bg-gray-700 flex items-center justify-center text-xs">N/A</div>
+                                            <span>N/A</span>
                                         @endif
                                     </td>
-                                    <td class="py-4 px-6 font-medium text-gray-900 dark:text-white">{{ $namakeluarga->NAMA }}</td>
-                                    <td class="py-4 px-6">{{ $namakeluarga->NAMA_AYAH }}</td>
-                                    <td class="py-4 px-6">{{ $namakeluarga->hubungan_ayah }}</td>
-                                    <td class="py-4 px-6">{{ $namakeluarga->NAMA_IBU }}</td>
-                                    <td class="py-4 px-6">{{ $namakeluarga->hubungan_ibu }}</td>
                                     <td class="py-4 px-6">
                                         <div class="flex justify-center items-center space-x-2">
-                                            <a href="{{ route('namakeluarga.edit', $namakeluarga->id) }}" class="p-2 bg-yellow-500 text-white rounded-md hover:bg-yellow-600" title="Edit">
+                                            <a href="{{ route('namakeluarga.edit', $item->id) }}" class="p-2 bg-yellow-500 text-white rounded-md hover:bg-yellow-600" title="Edit">
                                                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.5L16.732 3.732z"></path></svg>
                                             </a>
-                                            <form action="{{ route('namakeluarga.destroy', $namakeluarga->id) }}" method="POST" onsubmit="return confirm('Apakah Anda yakin ingin menghapus data Keluarga mahasiswa ini?');">
+                                            <form action="{{ route('namakeluarga.destroy', $item->id) }}" method="POST" onsubmit="return confirm('Apakah Anda yakin ingin menghapus data Keluarga mahasiswa ini?');">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="submit" class="p-2 bg-red-500 text-white rounded-md hover:bg-red-600" title="Hapus">
@@ -71,7 +72,8 @@
                                 </tr>
                                 @empty
                                 <tr>
-                                    <td colspan="8" class="py-4 px-6 text-center text-gray-500 dark:text-gray-400">
+                                    {{-- [PERBAIKAN] Colspan disesuaikan menjadi 7 --}}
+                                    <td colspan="7" class="py-4 px-6 text-center text-gray-500 dark:text-gray-400">
                                         Data Keluarga Mahasiswa belum tersedia.
                                     </td>
                                 </tr>
@@ -87,3 +89,4 @@
         </div>
     </div>
 </x-app-layout>
+
